bf202f4f53d9117f3a12e6d023e59bc5defc3359
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-04-14 23:47:10 -0700

    Delete Functionality
    Adding the ability to delete transaction based on the id of the transaction.  Creating action Delete transaction that will alter the state of the app

--- src/context/AppReducer.js
+++ src/context/AppReducer.js
@@ -1,8 +1,22 @@
 // Reducer how we specify the  application state changes based on user actions to the context or store
-
+// change your state and send down to component
 export default (state, action) => {
   switch (action.type) {
+    case 'DELETE_TRANSACTION':
+      return {
+        ...state,
+        transactions: state.transactions.filter(
+          (transaction) => transaction.id !== action.payload
+        ),
+      };
+    case 'ADD_TRANSACTION':
+      return {
+        ...state,
+        transactions: [action.payload, ...state.transactions],
+      };
     default:
       return state;
   }
 };
+
+//action payload is the new transaction being added to the total
--- src/context/GlobalState.js
+++ src/context/GlobalState.js
@@ -26,8 +26,29 @@ export const GlobalContext = createContext(initialState);
 // whenever we want to call reducer we have to have the dispatch called
 export const GlobalProvider = ({ children }) => {
   const [state, dispatch] = useReducer(AppReducer, initialState);
+
+  // Actions
+  function deleteTransaction(id) {
+    dispatch({
+      type: 'DELETE_TRANSACTION',
+      payload: id,
+    });
+  }
+  function addTransaction(transaction) {
+    dispatch({
+      type: 'ADD_TRANSACTION',
+      payload: transaction,
+    });
+  }
+
   return (
-    <GlobalContext.Provider value={{ transactions: state.transactions }}>
+    <GlobalContext.Provider
+      value={{
+        transactions: state.transactions,
+        deleteTransaction,
+        addTransaction,
+      }}
+    >
       {children}
     </GlobalContext.Provider>
   );
