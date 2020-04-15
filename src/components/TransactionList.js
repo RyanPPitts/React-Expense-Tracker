27468b630bdce9e10106c6175a785ab3fa08b03f
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-04-14 22:22:03 -0700

    Transaction List Update
    Importing useContext and GlobalContext so component has access to the globalstate of the app.  This way the transactions can be applied to the list within the global context

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext, GlobalProvider } from '../context/GlobalState';
+// import GlobalContext so this component can use the global state
 
 export const TransactionList = () => {
+  const { transactions } = useContext(GlobalContext);
+  //   const context = useContext(GlobalContext);
+  //   console.log(context.transactions); // pulling the globalstate information for this component
+
   return (
     <>
       <h3> History</h3>
       <ul className="list">
-        <li className="minus">
-          Cash<span>-$400</span>
-          <button className="delete-btn">x</button>
-        </li>
+        {transactions.map((transaction) => (
+          <li className="minus">
+            {transaction.text}
+            <span>-$400</span>
+            <button className="delete-btn">x</button>
+          </li>
+        ))}
       </ul>
     </>
   );
