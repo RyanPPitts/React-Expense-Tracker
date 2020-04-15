3f2abeb75cb3a162ec14d9deb95436fde21d9dbf
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-04-14 21:29:33 -0700

    Income/Transaction Components Built

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { Header } from './components/Header';
 import { Balance } from './components/Balance';
+import { IncomeExpenses } from './components/IncomeExpenses';
 import './App.css';
+import { TransactionList } from './components/TransactionList';
 
 function App() {
   return (
     <div>
       <Header />
       <Balance />
+      <IncomeExpenses />
+      <TransactionList />
     </div>
   );
 }
