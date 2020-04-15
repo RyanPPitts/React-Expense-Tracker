b3cd8eaa90fdb72a413fe54a5479d58c5e30b38d
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-04-14 21:34:30 -0700

    AddTransaction Component Added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import './App.css';
 import { TransactionList } from './components/TransactionList';
+import { AddTransaction } from './components/AddTransaction';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Balance />
       <IncomeExpenses />
       <TransactionList />
+      <AddTransaction />
     </div>
   );
 }
--- src/components/Balance.js
+++ src/components/Balance.js
@@ -5,7 +5,7 @@ export const Balance = () => {
     <>
       {/* using react fragment on line 5 and 9 */}
       <h4>Balance</h4>
-      <h1 id="balance">$0.00</h1>
+      <h1>$0.00</h1>
     </>
   );
 };
