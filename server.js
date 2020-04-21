016fe35e1076caa229ef6f6ac89a46c99e4a438d
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-04-20 20:36:19 -0700

    Adding DB connection with mongoose

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
+const connectDB = require('./config/db.js');
 // dependencies are brought in
 
 dotenv.config({ path: './config/config.env' });
