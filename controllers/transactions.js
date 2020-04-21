00806e719792f4bbda9948fbed972a8065ff4b44
Ryan Pitts <47765262+RyanPPitts@users.noreply.github.com>
2020-04-20 20:14:43 -0700

    Adding controllers
    Add, Get and Delete

--- controllers/transactions.js
+++ controllers/transactions.js
@@ -5,3 +5,19 @@
 exports.getTransactions = () => (req, res, next) => {
   res.send('GET transactions');
 };
+
+// @desc Add a transactions
+// @route POST /api/v1/transactions
+// @access Public
+
+exports.addTransactions = () => (req, res, next) => {
+  res.send('POST transactions');
+};
+
+// @desc Deletetransactions
+// @route DELETE /api/v1/transactions/:id
+// @access Public
+
+exports.deleteTransactions = () => (req, res, next) => {
+  res.send('DELETE transactions');
+};
